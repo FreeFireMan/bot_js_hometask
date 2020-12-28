86efbe9070268dea294d3a3477bfb805fbce92e8
freefireman <beluh.c.s@gmail.com>
2020-12-28 21:26:35 +0200

    some fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const config = {
     target: "node",
     output: {
         path: path.join(CURRENT_WORKING_DIR , '/dist/'),
-        filename: "server.generated.js",
+        filename: "js_bot.js",
         publicPath: '/dist/',
         libraryTarget: "commonjs2"
     },
